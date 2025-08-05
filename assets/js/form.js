f24019d44146e649edb3dd48a5e6735979f4d4d7
Aidan Liu <boomcreeper296@gmail.com>
2025-08-05 16:07:28 -0400

    Do not redirect to Formspree on form submit

---
+++ assets/js/form.js
@@ -0,0 +1,44 @@
+addEventListener('submit', (event) => onSubmit(event))
+
+function hideForm() {
+    const form = document.getElementById("contactform");
+    const wrapper = document.getElementById("contactformwrapper")
+    const text = document.createTextNode("Form submitted. Thank you!")
+    form.style.display = "none";
+    wrapper.append(text)
+}
+
+let hasError = false;
+function errorMessage(string) {
+    if (hasError) return
+    const form = document.getElementById("contactform");
+    const text = document.createTextNode(string)
+    form.prepend(text)
+    hasError = true
+}
+
+function onSubmit(event) {
+    event.preventDefault()
+    if (event.target.id === "contactform") {
+        const data = new FormData(event.target)
+
+        fetch("https://formspree.io/f/xqalyqka", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json"
+            },
+            body: data,
+        }).then(r => {
+            if (r.ok || r.status === 302) {
+                hideForm()
+            } else if (r.status === 422) {
+                errorMessage("Something went wrong. Did you type in your email address correctly?")
+                console.log(r)
+            } else {
+                errorMessage("Something went wrong, and your message has not been sent.")
+                console.log(r)
+            }
+        })
+    }
+}
+
